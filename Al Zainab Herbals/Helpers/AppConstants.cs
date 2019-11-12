b87691e20a86086ca36acdc86dd75dc02fc40286
WASEEM ABBAS <wajutt8@gmail.com>
2019-11-12 23:14:41 +0500

    Add project files.

---
+++ Al Zainab Herbals/Helpers/AppConstants.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Al_Zainab_Herbals.Helpers
+{
+    public class AppConstants
+    {
+        public const string ORG_FULL_NAME = "Alzainab Herbals";
+
+        public const string ORG_ADDRESS = "Alzainab Street, Mian Saeed Colony near Capital Road, Sialkot";
+        public const string CONTACT_PHONE_PRIMARY = "0092-334-9497381";
+        public const string CONTACT_PHONE_SECONDARY = "0092-300-6143578";
+        public const string CONTACT_EMAIL = "info@alzainabherbals.com";
+
+        public const string URL_FACEBOOK = "";
+        public const string URL_GOOGLE = "";
+
+
+        public const string MSG_SAVED_SUCCESS = "Data saved Succefully!";
+        public const string MSG_SAVE_ERROR = "Some Error Occoured while saving data.";
+    }
+}
---
+++ Al Zainab Herbals/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Al_Zainab_Herbals.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
